f4ddc1f9f07b4be060293b1f9964ef0643fab49b
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-03-13 15:12:59 +0700

    Заготовка обработки ключей запуска

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# post-ide-dsm-manager
- Manager for PoST IDE modules
+#post-ide-dsm-manager
+Manager for PoST IDE modules
+ 
+#Use examples
+
+java -jar ./post-ide-dsm-manager-1.0.0.jar -help
+java -jar ./post-ide-dsm-manager-1.0.0.jar -host 127.0.0.1 -port 123
--- src/main/java/su/nsk/iae/post/dsm/manager/Application.java
+++ src/main/java/su/nsk/iae/post/dsm/manager/Application.java
@@ -1,7 +1,39 @@
 package su.nsk.iae.post.dsm.manager;
 
 public class Application {
+	
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 8080;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello there!");
+		System.out.println("PoST IDE DSM-manager:\tuse -help to see available running configurations");
+		
+	    String host = DEFAULT_HOST;
+	    int port = DEFAULT_PORT;
+	    
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            switch (arg) {
+            	case "-help":
+            		help();
+            		return;
+                case "-port":
+                	i++;
+                    port = Integer.parseInt(args[i]);
+                    break;
+                case "-host":
+                	i++;
+                    host = args[i];
+                    break;
+            }
+        }
+        
+        System.out.println("PoST IDE DSM-manager:\tstarting on " + host + ":" + port + "...");
+	}
+	
+	private static void help() {
+		System.out.println("PoST IDE DSM-manager:\tavailable running configurations:");
+		System.out.println("\t-host (default \"localhost\")");
+		System.out.println("\t-port (default 8080)");
 	}
 }
