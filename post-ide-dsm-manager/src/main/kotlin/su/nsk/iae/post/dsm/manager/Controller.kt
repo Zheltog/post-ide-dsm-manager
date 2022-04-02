b2162de79c1e10718a7906a78c8723e5c3def004
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-04-02 18:08:17 +0700

    Запуск модуля: get -> post

--- post-ide-dsm-manager/src/main/kotlin/su/nsk/iae/post/dsm/manager/Controller.kt
+++ post-ide-dsm-manager/src/main/kotlin/su/nsk/iae/post/dsm/manager/Controller.kt
@@ -39,7 +39,7 @@ class Controller {
         return ModulesListResponse(Manager.getModules())
     }
 
-    @GetMapping(value = ["run/{moduleName}"])
+    @PostMapping(value = ["run/{moduleName}"])
     fun runModule(
         @PathVariable moduleName: String,
         @RequestBody request: DsmRequestBody
