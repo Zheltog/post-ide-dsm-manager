5fa6c1de974c1a5dab0f0fe38c14387c600aa38a
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-04-20 17:05:52 +0700

    Инвертировал ключ: don't-start-available-modules -> start-available-modules

--- post-ide-dsm-manager/src/main/kotlin/su/nsk/iae/post/dsm/manager/App.kt
+++ post-ide-dsm-manager/src/main/kotlin/su/nsk/iae/post/dsm/manager/App.kt
@@ -14,17 +14,17 @@ fun main(args: Array<String>) {
         "use -help to see available running configurations"
     )
     var availableModulesJson: String? = null
-    var startAvailableModules = true
+    var startAvailableModules = false
     for (i in args.indices) {
         if ("-help" == args[i]) {
             help()
             return
         }
-        if ("-am" == args[i]) {
+        if ("-amj" == args[i]) {
             availableModulesJson = args[i+1]
         }
-        if ("-dsam" == args[i]) {
-            startAvailableModules = false
+        if ("-sam" == args[i]) {
+            startAvailableModules = true
         }
     }
     runApplication<App>(*args)
@@ -37,6 +37,6 @@ fun main(args: Array<String>) {
 private fun help() {
     Logger.info(App::class.java, "available running configurations:")
     Logger.info(App::class.java, "java -Dserver.port=<port-number> -jar <jar name>")
-    Logger.info(App::class.java, "java -jar <jar name> -am <available-modules.json filepath> (taking from resource folder by default)")
-    Logger.info(App::class.java, "java -jar <jar name> -dsam (don't start available modules)")
+    Logger.info(App::class.java, "java -jar <jar name> -amj <available-modules.json filepath> (taking from resource folder by default)")
+    Logger.info(App::class.java, "java -jar <jar name> -sam (start available modules)")
 }
